b39d1b923dbc894b5f7b65870df1adbaf18962b2
nicolevenachi <ludwing@unicauca.edu.co>
2023-10-12 06:51:01 -0500

    about-2, customization

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Tilt from "react-tilt";
+import Tilt from "react-tilt"; //para efecto de tilt/inclinacion en las cartas 3when hover
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
@@ -9,6 +9,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className='xs:w-[250px] w-full'>
+    {/* tilt para la card y tambien una transicion de fade*/}
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -19,6 +20,7 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450,
         }}
+        // tilt options d  la card
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
       >
         <img
@@ -26,10 +28,12 @@ const ServiceCard = ({ index, title, icon }) => (
           alt='web-development'
           className='w-16 h-16 object-contain'
         />
+        {/* icono de la skill/service */}
 
         <h3 className='text-white text-[20px] font-bold text-center'>
           {title}
         </h3>
+        {/* titulo de la kill */}
       </div>
     </motion.div>
   </Tilt>
@@ -42,23 +46,22 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
+      {/* head con animated text */}
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I’m an electronic engineer passionate about software development. 1.5 years of hands-on experience in front-end development, designing and implementing user interfaces using React and Redux. Focused on building intuitive and accessible web applications . In my free time, I enjoy learning new technologies and developing machine-learning solutions for diverse situations I face in my daily life.
       </motion.p>
+      {/* body, con una animacioncita pa qeu aparezca como de un desvanecido*/}
 
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
       </div>
+      {/* todos mis services */}
     </>
   );
 };
